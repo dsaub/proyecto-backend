26ef0305dbd112937d65f440309e341b39f40ee2
Daniel <danilacasito8@gmail.com>
2025-10-23 11:53:35 +0200

    Add annotation to DataInitializer

--- src/main/java/me/elordenador/proyectobackend/config/DataInitializer.java
+++ src/main/java/me/elordenador/proyectobackend/config/DataInitializer.java
@@ -5,10 +5,12 @@ import me.elordenador.proyectobackend.models.User;
 import me.elordenador.proyectobackend.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
+@Configuration
 public class DataInitializer {
     @Bean
     CommandLineRunner init(UserRepository userRepository, PasswordEncoder passwordEncoder) {
