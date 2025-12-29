a3ea167c5671ec72e80c8f1c958ba97ea5ddb5c2
Daniel <danilacasito8@gmail.com>
2025-12-29 17:37:44 +0100

    Write some product tests

--- src/test/java/me/elordenador/proyectobackend/service/ProductServiceTest.java
+++ src/test/java/me/elordenador/proyectobackend/service/ProductServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -50,10 +51,38 @@ public class ProductServiceTest {
 
     @Test
     void testGetById() {
-        Product result = productService.getById(product1.getId());
-        assertNotNull(result);
-        assertEquals("Acer Aspire X3990", result.getName());
-        assertEquals(product1.getId(), result.getId());
+        Product result1 = productService.getById(product1.getId());
+        assertNotNull(result1);
+        assertEquals(product1.getName(), result1.getName());
+        assertEquals(product1.getId(), result1.getId());
+        assertEquals(product1.getPrice(), result1.getPrice());
+        Product result2 = productService.getById(product2.getId());
+        assertNotNull(result2);
+        assertEquals(product2.getName(), result2.getName());
+        assertEquals(product2.getId(), product2.getId());
+        assertEquals(product2.getPrice(), result2.getPrice());
+    }
 
+    @Test
+    void testGetByName() {
+        Product result1 = productService.getByName(product1.getName());
+        Product result2 = productService.getByName(product2.getName());
+
+        assertNotNull(result1);
+        assertNotNull(result2);
+
+        assertEquals(product1.getId(), result1.getId());
+        assertEquals(product1.getName(), result1.getName());
+        assertEquals(product1.getPrice(), result1.getPrice());
+
+        assertEquals(product2.getId(), result2.getId());
+        assertEquals(product2.getName(), result2.getName());
+        assertEquals(product2.getPrice(), result2.getPrice());
+    }
+
+    @Test
+    void testGetNonExistingProducct() {
+        Product result = productService.getById(111111L);
+        assertNull(result);
     }
 }
