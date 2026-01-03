e6fa91b1daee64131a2245728f469d8cf26d605f
daniel <danilacasito8@gmail.com>
2026-01-03 14:07:23 +0100

    Add Image entity with fields for original name, file name, and MIME type

---
+++ src/main/java/me/elordenador/proyectobackend/models/Image.java
@@ -0,0 +1,20 @@
+package me.elordenador.proyectobackend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "images")
+@Getter
+@Setter
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String originalName;
+    private String fileName;
+    private String mimeType;
+
+}
